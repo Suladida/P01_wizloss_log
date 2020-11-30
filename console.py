f1c73184722b26f7c8497a9ed96d1946654bf350
Robyn Tyler <robynstyler@outlook.com>
2020-11-30 23:07:32 +0000

    imported models to console, created template lines to create instances.

---
+++ console.py
@@ -0,0 +1,28 @@
+import pdb
+from models.wizard import Wizard
+from models.item import Item
+from models.loss import Loss
+
+# wizard1 = Wizard("", "", "")
+# wizard2 = Wizard("", "", "")
+# wizard3 = Wizard("", "", "")
+
+# hat1 = Item("", "", "", "")
+# hat2 = Item("", "", "", "")
+# hat3 = Item("", "", "", "")
+# slippers1 = Item("", "", "", "")
+# slippers2 = Item("", "", "", "")
+# slippers3 = Item("", "", "", "")
+# wand1 = Item("", "", "", "")
+# wand2 = Item("", "", "", "")
+# wand3 = Item("", "", "", "")
+
+# loss1 = Loss("", "", "", "", "", "")
+# loss2 = Loss("", "", "", "", "", "")
+# loss3 = Loss("", "", "", "", "", "")
+# loss4 = Loss("", "", "", "", "", "")
+# loss5 = Loss("", "", "", "", "", "")
+# loss6 = Loss("", "", "", "", "", "")
+# loss7 = Loss("", "", "", "", "", "")
+
+pdb.set_trace()
