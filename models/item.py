996bc02317ae615558f18b51cf038a5494778d84
Robyn Tyler <robynstyler@outlook.com>
2020-11-30 23:00:54 +0000

    Created Models Wizard, Item and Loss.

---
+++ models/item.py
@@ -0,0 +1,8 @@
+class Item:
+    
+    def __init__(self, type, colour, style, wizard_id, id = None):
+        self.type = type
+        self.colour = colour
+        self.style = style
+        self.wizard_id = wizard_id
+        self.id = id
---
+++ models/wizard.py
@@ -0,0 +1,8 @@
+class Wizard:
+    
+    def __init__(self, first_name, last_name, age, id = None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.id = id
+
