8492a895d763a65139712f8aa7f67dbc1e5b38dc
Robyn Tyler <robynstyler@outlook.com>
2020-11-30 23:15:47 +0000

    Connected to database.

--- db/wiz_loss.sql
+++ db/wiz_loss.sql
@@ -24,6 +24,6 @@ CREATE TABLE losses (
     time TIME,
     details VARCHAR(255),
     wizard_id INT REFERENCES wizards(id),
-    item_id INT REFERENCES item(id),
+    item_id INT REFERENCES items(id),
     recovered BOOLEAN 
 );
