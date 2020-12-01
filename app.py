20165a318eaacb40289c32e91ec0f61483b987d1
Robyn Tyler <robynstyler@outlook.com>
2020-12-01 16:57:20 +0000

    Blueprint added, Working with Wizard

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+from controllers import wizards_controller
+from controllers.wizards_controller import wizards_blueprint
 
 app = Flask(__name__)
 
-from controllers import wizards_controller
+app.register_blueprint(wizards_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- controllers/wizards_controller.py
+++ controllers/wizards_controller.py
@@ -1,10 +1,10 @@
 from flask import Flask, redirect, render_template, request, Blueprint
-from app import app
 from models.wizard import Wizard
 import repositories.wizard_repository as wiz_repo
 
+wizards_blueprint = Blueprint('wizards', __name__)
 
-@app.route('/')
+@wizards_blueprint.route('/')
 def home():
     wizards = wiz_repo.select_all()
     return render_template('wizards/wizards.html', title = 'Home', wizards=wizards)
@@ -13,20 +13,20 @@ def home():
 # def greet(name): 
 #     return f"Hello {name}!"
 
-@app.route('/new')
-def new():
-    return render_template('wizards/new.html')
+# @app.route('/new')
+# def new():
+#     return render_template('wizards/new.html')
 
-@app.route('/edit')
-def edit():
-    return render_template('wizards/edit.html')
+# @app.route('/edit')
+# def edit():
+#     return render_template('wizards/edit.html')
 
-@app.route('/wizards', methods=['POST', 'GET'])
-def add_wizard():
-    wizFirstName = request.form['first_name']
-    wizLastName = request.form['last_name']
-    wizAge = request.form['age']
-    newWizard = Wizard(first_name = wizFirstName, last_name = wizLastName, age = wizAge)
-    wiz_repo.save(newWizard)
-    wizards = wiz_repo.select_all()
-    return render_template('wizards/wizards.html', title='Home', wizards=wizards)
+# @app.route('/wizards', methods=['POST', 'GET'])
+# def add_wizard():
+#     wizFirstName = request.form['first_name']
+#     wizLastName = request.form['last_name']
+#     wizAge = request.form['age']
+#     newWizard = Wizard(first_name = wizFirstName, last_name = wizLastName, age = wizAge)
+#     wiz_repo.save(newWizard)
+#     wizards = wiz_repo.select_all()
+#     return render_template('wizards/wizards.html', title='Home', wizards=wizards)
