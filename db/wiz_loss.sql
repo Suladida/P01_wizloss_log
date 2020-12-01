07841a307ba4d88cdd2f29321e7d1f7e8294e93f
Robyn Tyler <robynstyler@outlook.com>
2020-12-01 10:14:51 +0000

    Instances Added.

--- db/wiz_loss.sql
+++ db/wiz_loss.sql
@@ -21,7 +21,6 @@ CREATE TABLE items (
 CREATE TABLE losses (
     id SERIAL PRIMARY KEY,
     date DATE,
-    time TIME,
     details VARCHAR(255),
     wizard_id INT REFERENCES wizards(id),
     item_id INT REFERENCES items(id),
--- models/loss.py
+++ models/loss.py
@@ -1,8 +1,7 @@
 class Loss:
     
-    def __init__(self, date, time, details, wizard_id, item_id, recovered = False, id = None):
+    def __init__(self, date, details, wizard_id, item_id, recovered = False, id = None):
         self.date = date
-        self.time = time
         self.details = details
         self.wizard_id = wizard_id
         self.item_id = item_id
