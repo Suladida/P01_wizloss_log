a82dd35af85353d461061ceb859324eb6ce49240
Robyn Tyler <robynstyler@outlook.com>
2020-12-01 10:51:03 +0000

    Added pseudocode to repos.

--- repositories/item_repository.py
+++ repositories/item_repository.py
@@ -2,3 +2,20 @@ from db.run_sql import run_sql
 from models.item import Item
 import repositories.item_repository as item_repo
 
+# CREATE:
+# save function goes here
+
+# READ_ALL:
+# select_all function goes here
+
+# READ:
+# select function goes here
+
+# UPDATE:
+# - update function goes here
+
+# DELETE:
+# - delete function goes here
+
+# DELETE_ALL:
+# - delete_all function goes here
