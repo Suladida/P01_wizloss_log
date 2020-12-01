7893bf485b8325705ca33ca497cb0ada65243a25
Robyn Tyler <robynstyler@outlook.com>
2020-12-01 13:50:20 +0000

    Modified app route to include wizards select_all

--- controllers/wizards_controller.py
+++ controllers/wizards_controller.py
@@ -1,9 +1,12 @@
 from flask import Flask, redirect, render_template, Blueprint
 from app import app
+from models.wizard import Wizard
+import repositories.wizard_repository as wiz_repo
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    wizards = wiz_repo.select_all()
+    return render_template('wizards/index.html', title = 'Home', wizards=wizards)
 
 @app.route('/<name>') 
 def greet(name): 
