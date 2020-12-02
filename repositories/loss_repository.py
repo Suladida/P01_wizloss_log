6f4f48173f219d172749b723bd0737c1c03721e1
Robyn Tyler <robynstyler@outlook.com>
2020-12-02 19:36:19 +0000

    More junk.

--- repositories/loss_repository.py
+++ repositories/loss_repository.py
@@ -53,7 +53,6 @@ def update(loss):
     values = [loss.day, loss.month, loss.year, loss.details, loss.wizard.id, loss.item.id, loss.recovered, loss.id]
     print("Hey")
     run_sql(sql, values)
-    print(f"✅ Loss {loss.item.id} Updated: {loss.item.type} {loss.day} {loss.month} {loss.year} {loss.details} {loss.wizard.id} {loss.item.id} {loss.recovered}")
 
 # DELETE:
 # - delete function goes here
@@ -81,7 +80,6 @@ def active_losses():
         item = item_repo.select(row['item_id'])
         loss = Loss(row['day'], row['month'], row['year'], row['details'], wizard, item, row['recovered'], row['id'] )
         losses.append(loss)
-        print(f"🧙‍♂️ Success! {item.id} {item.type} {item.colour} {item.style}") 
     return losses
 
 def current_losses(wizard):
@@ -96,7 +94,6 @@ def current_losses(wizard):
         item = item_repo.select(row['item_id'])
         loss = Loss(row['day'], row['month'], row['year'], row['details'], wizard, item, row['recovered'], row['id'] )
         losses.append(loss)
-        print(f"🧙‍♂️ Oh Dear! {wizard.first_name} {wizard.last_name} HAS LOST {item.colour}{item.type}{item.style}") 
     return losses
 
 def loss_history(wizard):
@@ -111,36 +108,15 @@ def loss_history(wizard):
         item = item_repo.select(row['item_id'])
         loss = Loss(row['day'], row['month'], row['year'], row['details'], wizard, item, row['recovered'], row['id'] )
         losses.append(loss)
-        print(f"🧙‍♂️⛔️⏳ {loss.day} / {loss.month} / {loss.year} - WIZARD: {wizard.first_name} {wizard.last_name} - LOST: {item.colour} {item.type} {item.style}") 
     return losses
 
 def check_item_status(item):
     sql = ("select * from losses where recovered = False AND item_id = %s")
     value = [item.id]
     result = run_sql(sql, value)
-    # sql = "SELECT items.* FROM items INNER JOIN losses ON losses.item_id = items.id WHERE item_id = %s AND recovered = False"
-    # values = [item.id]
-    # result = run_sql(sql, values)
-
-# SELECT * from items where item.id = FALSE
-
-    # for row in results:
-    #     wizard = wiz_repo.select(row['wizard_id'])
-    #     item = item_repo.select(row['item_id'])
-    #     loss = Loss(row['day'], row['month'], row['year'], row['details'], wizard, item, row['recovered'], row['id'] )
-    #     losses.append(loss)
-    #     print(f"🧙‍♂️ Oh Dear! {wizard.first_name} {wizard.last_name} HAS LOST {item.colour}{item.type}{item.style}") 
-    # return losses
-    # for item in result:
-    # else: 
-    # for result in results:
     if result is not None:
         print(f"🧙‍♂️ {item.colour} {item.type} is lost ⛔️") 
     return False
-    # else:
-        # print(f"🧙‍♂️ {item.colour} {item.type} is not lost ✅")
-    #     status = True
-
 
 # update item as recovered
 
@@ -151,22 +127,3 @@ def mark_item_recovered(loss):
     run_sql(sql, values)
     print(f"Recovered ✅ Item recovered is now: {loss.recovered}")
     return True
-    # print(f"{item.wizard.first_name} {item.wizard.last_name}'s {item.type} has been updated to Recovered ✅")
-
-
-
-
-
-
-# checks if a wizard owns an item and it's not lost (i.e. current inventory)
-
-# def wizard_inventory(wizard, item):
-# inventory = []
-# ### checks if wizard owns item
-# owned = check_wizard_owns_item(wizard, item)
-# ### checks if item is NOT lost
-# status = check_item_status(item)
-# For item in inventory:
-# If owned = true and status = true: 
-# inventory.append(item)
-# return inventory
